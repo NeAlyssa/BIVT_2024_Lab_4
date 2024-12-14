088ddd5171d2d9e54c173b4f8e4bfef57b5f42e0
Kirill Samsonnik <samsonnik.kirill@gmail.com>
2024-12-15 00:03:27 +0300

    Add files via upload

--- Lab4/Program.cs
+++ Lab4/Program.cs
@@ -18,10 +18,13 @@ public class Program
     {
         int answer = 0;
         // code here
-
+        for (int i = 0; i < A.GetLength(0); i++)
+            for (int j = 0; j < A.GetLength(1); j++)
+                answer += A[i, j];
+        
         // end
 
-        return answer;
+                return answer;
     }
     public double Task_1_2(int[,] A)
     {
@@ -45,7 +48,13 @@ public class Program
     {
         int row = 0, col = 0;
         // code here
-
+        for (int i = 0; i < A.GetLength(0); i++)
+            for (int j = 0; j < A.GetLength(1); j++)
+                if (A[i,j] < A[row,col])
+                {
+                    row = i;
+                    col = j;
+                }
         // end
 
         return (row, col);
@@ -54,10 +63,9 @@ public class Program
     {
         int value = 0, rowIndex = -1;
         // code here
+                // end
 
-        // end
-
-        return (value, rowIndex);
+                return (value, rowIndex);
     }
     public int[] Task_1_6(int[,] A)
     {
@@ -71,8 +79,18 @@ public class Program
     public int[] Task_1_7(int[,] A)
     {
         int[] answer = default(int[]);
+        if (A.GetLength(0) != 3 || A.GetLength(1) != 5) return answer;
+        answer = new int[A.GetLength(1)];
         // code here
-
+        int m = -int.MaxValue;
+        for (int i = 0; i < A.GetLength(1); i++)
+        {
+            for (int j = 0; j < A.GetLength(0); j++)
+                if (A[j, i] > m)
+                    m = A[j, i];
+            answer[i] = m;
+            m = -int.MaxValue;
+        }
         // end
 
         return answer;
@@ -97,10 +115,16 @@ public class Program
     public int[,] Task_1_10(int[,] A)
     {
         // code here
+        if (A.GetLength(0) != 5 || A.GetLength(1) != 7) return null;
+        int m = 0;
+        for (int i = 0; i < A.GetLength(0); i++)
+            if (A[i, 2] > A[m, 2]) m = i;
+        if (m != 3)
+            for (int i = 0; i < A.GetLength(1); i++)
+                (A[m, i],A[3, i]) = (A[3, i], A[m, i]);
+                // end
 
-        // end
-
-        return A;
+                return A;
     }
     public int[,] Task_1_11(int[,] A)
     {
@@ -121,7 +145,13 @@ public class Program
     public int[,] Task_1_13(int[,] A)
     {
         // code here
-
+        if (A.GetLength(0) != 5 || A.GetLength(1) != 5) return null;
+        int m = 0;
+        for (int i = 0; i < A.GetLength(1); i++)
+            if (A[i, i] > A[m, m]) m = i;
+        if (m != 3)
+            for (int i = 0; i < A.GetLength(0); i++)
+                (A[i,m], A[i, 3]) = (A[i, 3], A[i, m]);
         // end
 
         return A;
@@ -130,7 +160,11 @@ public class Program
     {
         int[] answer = default(int[]);
         // code here
-
+        if (A.GetLength(0) != 4 || A.GetLength(1) != 3) return answer;
+        answer = new int[A.GetLength(1)];
+        for (int i = 0; i < A.GetLength(1); i++)
+            for (int j = 0; j < A.GetLength(0); j++)
+                if (A[j, i] < 0) answer[i]++;
         // end
 
         return answer;
@@ -146,7 +180,16 @@ public class Program
     public int[,] Task_1_16(int[,] A, int n, int m)
     {
         // code here
-
+        if (A.GetLength(0) != n || A.GetLength(1) != m) return null;
+        int b = 0;
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+                if (A[i, j] > A[i, b]) b = j;
+            for (int j = b; j < m - 1; j++)
+                (A[i, j], A[i, j + 1]) = (A[i, j + 1], A[i, j]);
+            b = 0;
+        }
         // end
 
         return A;
@@ -170,6 +213,15 @@ public class Program
     public double[,] Task_1_19(double[,] C, int n, int m)
     {
         // code here
+        if (C.GetLength(0) != n || C.GetLength(1) != m) return null;
+        for (int i = 0; i < n; i++)
+        {
+            int b = 0;
+            for (int j = 0; j < m; j++)
+                if (C[i, j] > C[i, b]) b = j;
+            for (int j = 0; j < b; j++)
+                if (C[i, j] < 0) C[i, j] /= C[i, b];
+        }
 
         // end
 
@@ -194,7 +246,20 @@ public class Program
     public double[,] Task_1_22(double[,] Z)
     {
         // code here
-
+        if (Z.GetLength(0) != 6 || Z.GetLength(1) != 8) return null;
+        double s = 0;
+        int a = 0, b = 0, k = 0;
+        for (int i = 0; i < Z.GetLength(0); i++)
+            for (int j = 0; j < Z.GetLength(1); j++)
+            {
+                if (Z[i, j] > 0)
+                {
+                    s += Z[i, j];
+                    k++;
+                }
+                if (Z[i, j] > Z[a, b]) { a = i; b = j; }
+            }
+        Z[a, b] = s / k;
         // end
 
         return Z;
@@ -218,10 +283,24 @@ public class Program
     public int[,] Task_1_25(int[,] X)
     {
         // code here
-
+        if (X.GetLength(0) != 6 || X.GetLength(1) != 5) return null;
+        int a = 0, b = 0, m = 0, n = X.GetLength(1), k = 0;
+        for (int i = 0; i < X.GetLength(0); i++)
+        {
+            for (int j = 0; j < X.GetLength(1); j++)
+                if (X[i, j] < 0)
+                    k++;
+            if (m < k)
+            { m = k; a = i; }
+            if (n > k)
+            { n = k; b = i; }
+            k = 0;
+        }
+        for (int i = 0; i < X.GetLength(1); i++)
+            (X[a, i], X[b, i]) = (X[b, i], X[a, i]);
         // end
 
-        return X;
+            return X;
     }
     public int[,] Task_1_26(int[,] A, int[] B)
     {
@@ -242,7 +321,26 @@ public class Program
     public int[,] Task_1_28(int[,] A)
     {
         // code here
+        if (A.GetLength(0) != 7 || A.GetLength(1) != 5) return null;
+        int s = 0, m = 0, a = 0;
+        for (int i = 0; i < A.GetLength(0); i++)
+        {
+            for (int j = 0; j < A.GetLength(1); j++)
+                if (A[i, j] > 0)
+                    s += A[i, j];
+            if (s > m) {m = s; a = i; }
+            s = 0;
+        }
 
+        int[,] t = new int [A.GetLength(0) - 1, A.GetLength(1)];
+        for (int i = 0; i < a; i++)
+            for (int j = 0; j < A.GetLength(1); j++)
+                t[i, j] = A[i, j];
+        for (int i = a; i < A.GetLength(0) - 1; i++)
+            for (int j = 0; j < A.GetLength(1); j++)
+                t[i, j] = A[i+1, j];
+        A = t;
+        Console.WriteLine(A);
         // end
 
         return A;
